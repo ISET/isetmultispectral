17fb35c921d25d2fc9ed246adb073d05eb0a03c7
Brian A. Wandell <wandell@stanford.edu>
2013-09-01 23:23:46 -0700

    Changed algorithm, removing tolerance.  Should probably put tolerance back in

--- mcCamera2CSBasis.m
+++ mcCamera2CSBasis.m
@@ -1,44 +1,75 @@
-function coef  = mcCamera2CSBasis(sensor, colorSignalBasis, mcHDRImage)
+function coef  = mcCamera2CSBasis(sensor, sigBasis, mcHDRRGB)
+% Compute the coefficients so that coef*sigBasis' approximates the photons.
 %
-%   coef = mcCamera2CSBasis(sensor, colorSignalBasis, mcHDRImage)
+%   coef = mcCamera2CSBasis(sensor, sigBasis, mcHDRRGB)
 %
-%Author: FX, BW
-%Purpose:
-%    Compute the basis coefficients from knowledge of 
-%    the sensor responsivities, sensor
-%    the color signal basis functions, colorSignalBasis,
-%    and the camera response, RGB.
+% sensor:   Sensor spectral response in columns
+% sigBasis: Signal basis functions
+% mcHDRRGB: Multicapture, HDR image in RGB format
 %
-
-% Form the matrix that maps basis coefficients into sensor responses.  
+% coef is returned in RGB format
 %
-% The color filters may not be very independent of one another.  So, we
-% take care to measure the singular coefficients
-% limit singularity problems, we only use these greater than 0.01 of the
-% max singular value.
+% We use the sensor responsivities, the color signal basis functions,
+% sigBasis, and the sensor responsivity. These equations are written as
 %
-%  These equations are written as RGB = sensor' * basis * coef
-%  To estimate coef, we compute assuming RGB in columns and coef in
-%  columns. Recall that the XW format, though, has the RGB and coef in
-%  rows.  So, we must account for this below.
+%         mcHDRXW = coef * sigBasis' * sensor;
+%
+% The image coefficients are returned in a form where this works
+%
+%    photons = imageLinearTransform(coef,sigBasis');
+%
+% The code in imageLinearTransform reads coef and sigBasis and computes 
+%    coef    = RGB2XWFormat(coef);
+%    photons = coef*sigBasis';
+%    photons = XW2RGBFormat(photons,r,c);
+%
+% So, here, we find
+%
+%         coef = mcHDRXW * pinv((sigBasis'*sensor),tol);
+%
+% Because the color filters are not always independent of one another we
+% take care to measure the singular coefficients limit singularity
+% problems, we only use these greater than 0.01 of the max singular value.
+%
+% (Maybe we should do this with a ridge regression instead).
+%
+% See also:  s_mcCombineColor
+%
+% FX/BW Copyright Imageval Consulting, LLC 2005
 
-% for ii=1:9, plot(sensor(:,ii)); set(gca,'ylim',[0 1]); pause; end
-% plot(wavelength,colorSignalBasis)
+%% Should check parameters
+if ieNotDefined('sensor'), error('Sensor sensitivities required.'); end
+if ieNotDefined('sigBasis'), error('Color signal basis required'); end
+if ieNotDefined('mcHDRRGB'), error('multicapture HDR RGB image required.'); end
 
-% The matrix cam2cs will convert a column vector of camera measurements
-% into the color signal coefficients.  So, if d is a column vector of
-% camera measurements, cam2cs*d is a set of coefficients.
-A = sensor' * colorSignalBasis;
-[s,v,d] = svd(A);
-tol = v(1)*0.01;
-cam2cs = pinv(A,tol);
+%% Convert the HDR image to XW format and apply the matrix
 
-[r,c,w] = size(mcHDRImage);
-XW = RGB2XWFormat(mcHDRImage);
+% If d is a row vector of camera data, such as in XW format
+% So, coef*sigBasis'*sensor  is the predicted camera data, mcHDRXW
+% Notice that coef*sigBasis' is the photon image and coef*sigBasis'*sensor
+% is the predicted camera data, mcHDRXW
 
-% Here is where we put XW data into rows, and then we put the coef values
-% back into rows when we are done.
-coef = (cam2cs*XW')';
+% We put the data into XW format
+[mcHDRXW,r,c] = RGB2XWFormat(mcHDRRGB);
+
+% Calculate coefs
+coef = mcHDRXW * pinv(sigBasis'*sensor);
+
+% predicted = coef*sigBasis'*sensor;
+% vcNewGraphWin; plot(predicted(:),mcHDRXW(:),'.')
+
+% Put coefs into RGB format 
 coef = XW2RGBFormat(coef,r,c);
 
-return;
+%% To check, put the coef into photon space and then multiply by sensor
+% This should get us the camera data
+%   predicted = imageLinearTransform(coef,sigBasis'*sensor);
+%   vcNewGraphWin; plot(predicted(:),mcHDRRGB(:),'.'); grid on; axis equal
+%
+% Another problem, however, is that we are getting solutions with negative
+% photons.
+%   spd = imageLinearTransform(coef,sigBasis');
+%   vcNewGraphWin; hist(spd(:),500);
+%   min(spd(:)), max(spd(:))
+
+end
